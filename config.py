b1911ef66d6f31ff4a4f42df80e8c0dd1e6be59c
Filmyrohesh <142705544+Filmyrohesh@users.noreply.github.com>
2024-04-18 17:20:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://defensive-ptarmigan-7508446728.koyeb.app/"
+        URL = "clone-file-store-3e59be9626c0.herokuapp.com"
     else:
-        URL = "https://defensive-ptarmigan-7508446728.koyeb.app/"
+        URL = "clone-file-store-3e59be9626c0.herokuapp.com"
 
 
 
