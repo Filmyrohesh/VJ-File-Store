7c5010556f362f01f98c538aeac948c51d7f749b
Filmyrohesh <142705544+Filmyrohesh@users.noreply.github.com>
2024-04-19 12:10:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "clone-file-store-3e59be9626c0.herokuapp.com"
+        URL = "https://clone-file-store-3e59be9626c0.herokuapp.com/"
     else:
-        URL = "clone-file-store-3e59be9626c0.herokuapp.com"
+        URL = "https://clone-file-store-3e59be9626c0.herokuapp.com/"
 
 
 
